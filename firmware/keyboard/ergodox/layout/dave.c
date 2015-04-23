b0023c55f790b3fb5fb22d1bcd4a9db58e04d345
David Wilhelm <dewilhelm@gmail.com>
2015-04-23 08:48:31 -0400

    added number pad layer

--- firmware/keyboard/ergodox/layout/dave.c
+++ firmware/keyboard/ergodox/layout/dave.c
@@ -51,22 +51,23 @@ static layout_t layout PROGMEM = {
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
    esc     , F1      , F2     , F3     , F4     , F5 , F6   , 
-   tab   , quote   , comma  , period , p      , y  , home , 
-   lpupo1l1 , a       , o      , e      , u      , i  , 
-   lpupo2l2 , semicol , q      , j      , k      , x  , tab  , 
-   guiL     , arrowL  , arrowU , arrowD , altL , 
-                                                               tab     , end  , 
-                                                               nop    , nop  , home , 
-                                                               shL2kcap, ctrlL, esc  , 
+   tab   , quote   , comma  , period , p      , y  , nop , 
+   ctrlL, a       , o      , e      , u      , i  , 
+   lpupo2l2, semicol , q      , j      , k      , x  , guiL , 
+   lpupo3l3, nop  , nop , nop , altL  ,
+
+                                                               home     , end  , 
+                                                               nop    , nop  , nop , 
+                                                               shL2kcap, lpupo1l1 , lpupo3l3, 
 // right hand ..... ......... ......... ......... ......... ......... .........
-            F7       , F8     , F9     , F10    , F11     , F12   , esc     , 
-            pageU    , f      , g      , c      , r       , l     , bs      , 
+            F7       , F8     , F9     , F10    , F11     , F12   , nop     , 
+            nop    , f      , g      , c      , r       , l     , esc      , 
                        d,     h      , t      , n      , s       , dash  , 
-            enter ,    b      , m      , w      , v       , z     , caps , 
-                                bs   , arrowD , arrowU , arrowR , guiR    , 
+            enter ,    b      , m      , w      , v       , z     , lpupo1l1 , 
+                                bs   , nop, nop , nop , nop , 
    pageU,    pageD,
-   del,      nop,      nop,
-   bs ,    enter,    space  ),
+   nop,      nop,      nop,
+   del,    enter,    space  ),
 
 // ............................................................................
 
@@ -76,19 +77,19 @@ static layout_t layout PROGMEM = {
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
   nop,   nop,   nop,   nop,   nop,   nop,   nop,
-  nop,   nop,   nop,   nop,   nop,   nop,   nop,
-  transp,   nop,   nop,   nop,   nop,   nop,
+  transp,   pound, at, percent,   grave,   nop,   nop,
+  transp,   amp,   pipe, exclam,   question,   nop,
   transp,   nop,   nop,   nop,   nop,   nop,   nop,
-  transp,   nop,   nop,   nop,   nop,
+  transp,   nop,   nop,   nop,   del,
                                                               transp,   transp,
                                                     transp,   transp,   transp,
                                                     transp,   transp,   transp,
 // right hand ..... ......... ......... ......... ......... ......... .........
             nop,   nop,   nop,    nop,    nop,    nop,   nop,
             nop,   tilde,    braceL,    braceR,   caret,     dollar,   bkslash,
-                      plus,     parenL ,   parenR,   equal,     semicol,      colon,
-            nop,   slash,    brktL,     brktR,    at,        exclam,   transp,
-                                transp,   transp,   transp,    transp,   transp,
+                    plus,    parenL,   parenR,   equal,     semicol,      colon,
+            bs,    slash,    brktL,     brktR,    at,        exclam,   at,
+                                bs,   transp,   transp,    transp,   transp,
   transp,   transp,
   transp,   transp,   transp,
   transp,   transp,   transp  ),
@@ -96,28 +97,50 @@ static layout_t layout PROGMEM = {
 // ............................................................................
 // ............................................................................
 
-    MATRIX_LAYER(  // layer 2 : symbols & number pad
+
+    MATRIX_LAYER(  // layer 2 : number pad
 // macro, unused,
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
-  nop,   nop,   nop,   nop,   nop,   nop,   nop,
-  nop,   nop,   nop,   nop,   nop,   nop,   nop,
-  transp,   nop,   nop,   nop,   nop,   nop,
-  transp,   nop,   nop,   nop,   nop,   nop,   nop,
-  transp,   nop,   nop,   nop,   nop,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,      ins,   transp,   transp,   transp,
+                                                              transp,   transp,
+                                                    transp,   transp,   transp,
+                                                    transp,   transp,   lpo2l2,
+// right hand ..... ......... ......... ......... ......... ......... .........
+            lpo1l1,   transp,   transp,    transp,    transp, transp,   transp,
+            transp,   asterisk,      1,        2,        3,     period,   transp,
+                      plus,        4,        5,        6,     0,   dash,
+            transp,   slash,        7,        8,        9,    comma,   transp,
+                                transp,   transp,   transp,    transp,   transp,
+  transp,   transp,
+  transp,   transp,   transp,
+  transp,   transp,        transp  ),
+// ............................................................................
+//
+    MATRIX_LAYER(  // layer 3 : navigation
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,      ins,   transp,   transp,   transp,
                                                               transp,   transp,
                                                     transp,   transp,   transp,
                                                     transp,   transp,   transp,
 // right hand ..... ......... ......... ......... ......... ......... .........
-            nop,   nop,   nop,    nop,    nop, nop,   nop,
-            nop,   pound,        1,        2,        3,     percent,   grave,
-                      amp,        4,        5,        6,     0,      pipe,
-            nop,   asterisk,        7,        8,        9,    question,   nop,
-                                nop,   nop,   nop,    nop,   nop,
+            lpo1l1,   transp,   transp,    transp,    transp, transp,   transp,
+            transp,   transp,        transp,        transp,        transp,     transp,   transp,
+                      transp,        arrowL,        arrowD,        arrowU,     arrowR,   transp,
+            transp,   transp,        home,        pageD,        pageU,    end,   transp,
+                                transp,   transp,   transp,    transp,   transp,
   transp,   transp,
   transp,   transp,   transp,
-  transp,   transp,   transp  ),
-
-// ............................................................................
+  transp,   transp,        transp  ),
 };
 
